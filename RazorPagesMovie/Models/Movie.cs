bf8c4b38645cf4e20247827eaa66817d26203f16
mbonner88 <matthewsbonner@comcast.net>
2023-05-01 17:01:01 -0400

    add space

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -6,6 +6,7 @@ public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
